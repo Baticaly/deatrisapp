ed5892cac0e2fe50f114efb18ea4065d752f956b
Baticaly <ta6nbe@gmail.com>
2022-10-15 12:06:29 +0200

    logo update

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
+router.get('/templogin', (req, res) => {
+    res.render('login')
+})
+
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
     res.render('dashboard', {
         user: req.user
